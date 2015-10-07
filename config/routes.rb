7da621400a81a1d362f3f209e023b1130bab1219
Mark Evangelista <mark172@me.com>
2015-10-07 15:07:43 -0700

    adding css

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
 
   get "/calendar" => "pages#calendar"
 
-  get "projects/email" => "projects#email"
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
