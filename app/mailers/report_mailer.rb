754a3f4804ce4ec6b658ea9d73298a7aee653dbb
Mark Evangelista <mark172@me.com>
2015-10-22 15:48:15 -0700

    modified mailer model

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,8 +1,10 @@
 class ReportMailer < ActionMailer::Base
 	default to: 'markevangelista@livenation.com'
 
+	@@engeineer_for_index = { 1 => "Mike Voss", 2 => "Jessica Yang", 3 => "Mark Evangelista", 4 => "Javier Palenzuela"}
+
 	def report_email(name, project, status, percentage, thisweek, nextweek)
-		@name = name
+		@name = @@engeineer_for_index[name]
 		@project = project
 		@status = status
 		@percentage = percentage
