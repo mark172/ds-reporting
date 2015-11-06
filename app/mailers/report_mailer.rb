a3bb4a4a4a0314db2e69896fd3297617221d80b2
Mark Evangelista <mark172@me.com>
2015-11-06 15:51:03 -0800

    added new oncall image

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,11 +1,12 @@
 class ReportMailer < ActionMailer::Base
 	default to: 'markevangelista@livenation.com'
 
+	project = Project.new
 
 	@@engineer_for_index = { 1 => "Mike Voss", 2 => "Jessica Yang", 3 => "Mark Evangelista", 4 => "Javier Palenzuela"}
 
 	def report_email(name, project, status, percentage, thisweek, nextweek)
-		@name = convert_engineer_to_string(name)
+		@name = project.text_for_engineer[name]
 		@project = project
 		@status = status
 		@percentage = percentage
@@ -16,8 +17,6 @@ class ReportMailer < ActionMailer::Base
 	end
 
 	def convert_engineer_to_string(name)
-		if name == 3
-			return "MARK"
-		end
+		thisname = @@engineer_for_index[name]
 	end
 end
